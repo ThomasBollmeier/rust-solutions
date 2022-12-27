4832e819a1cb62fd536cf2a2fdc60040a84d8b6c
Thomas Bollmeier <developer@thomas-bollmeier.de>
2022-12-27 21:12:28 +0100

    Configure argument parser

--- findr/src/lib.rs
+++ findr/src/lib.rs
@@ -1,14 +1,17 @@
 use std::error::Error;
 
-use clap::{Parser, command, crate_authors, crate_version};
+use clap::{Parser, command, crate_authors, crate_version, ValueEnum};
 use regex::Regex;
 
 pub type MyResult<T> = Result<T, Box<dyn Error>>;
 
-#[derive(Debug, Eq, PartialEq, Clone)]
+#[derive(Debug, Eq, PartialEq, Clone, ValueEnum)]
 enum EntryType {
+    #[value(name = "d")]
     Dir,
+    #[value(name = "f")]
     File,
+    #[value(name = "l")]
     Link,
 }
 
@@ -19,16 +22,29 @@ enum EntryType {
     about = "Rust version of find"
 )]
 pub struct Config {
+    #[arg(
+        value_name = "PATH",
+        help = "Search paths",
+        default_value = ".",
+        num_args = 1..,
+    )]
     paths: Vec<String>,
+
     #[arg(
         short = 'n',
         long = "name",
+        value_name = "NAME",
+        num_args = 1..,
+        value_parser = validate_regex,
     )]
     names: Vec<Regex>,
+
     #[arg(
         short = 't',
         long = "type",
-        value_parser = validate_entry_type,
+        value_name = "TYPE",
+        value_enum,
+        num_args = 1..,
     )]
     entry_types: Vec<EntryType>,
 }
@@ -39,20 +55,11 @@ pub fn get_args() -> Config {
 
 pub fn run(config: &Config) -> MyResult<()> {
 
-    println!("{:?}", config);
+    println!("{:#?}", config);
     Ok(())
 }
 
-fn validate_entry_type(s: &str) -> Result<EntryType, String> {
-    if s.len() != 1 {
-        return Err(format!("Unknown type: {}", s));
-    }
-
-    match s.chars().next().unwrap() {
-        'f' => Ok(EntryType::File),
-        'd' => Ok(EntryType::Dir),
-        'l' => Ok(EntryType::Link),
-        _ => Err(format!("Unknown type: {}", s)),
-    }
-
+fn validate_regex(s: &str) -> Result<Regex, String> {
+    println!("Validating: {}", s);
+    Regex::new(s).map_err(|_|{ format!("Invalid --name \"{}\"", s)})
 }
